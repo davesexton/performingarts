4297688d6cbf91b5f8791992bccd01a8c9b8263e
Dave Sexton <davesexton1@googlemail.com>
2013-08-14 18:41:26 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Performingarts::Application.config.secret_token = 'f12fd812e8d2097d4cf1bded436efccbce3b469c2bd1582ea4b9118593fcd79e3332818d95ca987ebde5897abd8cb56fd57cb20672df3049aa7dab46af723bab'
